2ba05cc1f69938b7f92802e1a2784e00faf3ad26
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2015-10-03 11:42:18 +0200

    add first dashboard model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from tastypie.utils.timezone import now
 
-# Create your models here.
+class DeviceType(models.Model):
+	name = models.CharField(max_length=20)
+
+class BusType(models.Model):
+	name = models.ForeignKey(DeviceType)
+
+class MeasureType(models.Model):
+	type = models.CharField(max_length=20)
+
+class Bus(models.Model):
+	name = models.CharField(max_length=20)
+	type = models.ForeignKey(BusType)
+
+class Sequence(models.Model):
+	name = models.CharField(max_length=20)
+	description = models.CharField(max_length=200, blank=True)
+	payload = models.CharField(max_length=100)
+
+class Device(models.Model):
+	name = models.CharField(max_length=20)
+	place = models.CharField(max_length=50)
+	bus = models.ManyToManyField(DeviceType, blank=True)
+	sequence = models.ManyToManyField(Sequence, blank=True)
+
+class Measure(models.Model):
+	type = models.ForeignKey(MeasureType)
+	device = models.ForeignKey(Device)
+	value = models.FloatField()
+	time = models.DateTimeField(default=now)
