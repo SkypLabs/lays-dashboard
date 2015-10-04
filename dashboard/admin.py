de3da0ce97ee4a9f24bff215da559f0da77bdceb
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2015-10-04 17:03:31 +0200

    add models to admin app

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from dashboard.models import DeviceType, BusType, MeasureType, Bus, Sequence, Device, Measure
 
-# Register your models here.
+admin.site.register(DeviceType)
+admin.site.register(BusType)
+admin.site.register(MeasureType)
+admin.site.register(Bus)
+admin.site.register(Sequence)
+admin.site.register(Device)
+admin.site.register(Measure)
