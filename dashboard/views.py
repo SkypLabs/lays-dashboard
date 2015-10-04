643c8ad0f70220b6afffade1f27888115af41f4b
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2015-10-04 16:41:06 +0200

    add basic views

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,2 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+def index(request):
+	return HttpResponse("It works")
+
+def rawdata(request):
+	return HttpResponse("It works")
