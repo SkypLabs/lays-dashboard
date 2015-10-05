4a0ba192a9df8124dd9ea93066dc1d0159e1cbf3
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2015-10-06 01:54:28 +0200

    custom admin forms

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
-from dashboard.models import DeviceType, BusType, MeasureType, Bus, Sequence, Device, Measure
+from .models import DeviceType, BusType, MeasureType, Bus, Sequence, Device, Measure
+
+class BusAdmin(admin.ModelAdmin):
+	list_display = ('name', 'type')
+
+class SequenceAdmin(admin.ModelAdmin):
+	list_display = ('name', 'description', 'payload')
+
+class DeviceAdmin(admin.ModelAdmin):
+	list_display = ('name', 'place', 'bus')
+
+class MeasureAdmin(admin.ModelAdmin):
+	list_display = ('value', 'type', 'device', 'time')
 
 admin.site.register(DeviceType)
 admin.site.register(BusType)
 admin.site.register(MeasureType)
-admin.site.register(Bus)
-admin.site.register(Sequence)
-admin.site.register(Device)
-admin.site.register(Measure)
+admin.site.register(Bus, BusAdmin)
+admin.site.register(Sequence, SequenceAdmin)
+admin.site.register(Device, DeviceAdmin)
+admin.site.register(Measure, MeasureAdmin)
--- dashboard/models.py
+++ dashboard/models.py
@@ -4,15 +4,27 @@ from tastypie.utils.timezone import now
 class DeviceType(models.Model):
 	name = models.CharField(max_length=20)
 
+	def __str__(self):
+		return self.name
+
 class BusType(models.Model):
 	name = models.ForeignKey(DeviceType)
+
+	def __str__(self):
+		return self.name.name
 
 class MeasureType(models.Model):
 	name = models.CharField(max_length=20)
 
+	def __str__(self):
+		return self.name
+
 class Bus(models.Model):
 	name = models.CharField(max_length=20)
 	type = models.ForeignKey(BusType)
+
+	def __str__(self):
+		return self.name
 
 	class Meta:
 		verbose_name_plural = "Buses"
@@ -21,7 +33,10 @@ class Device(models.Model):
 	name = models.CharField(max_length=20)
 	place = models.CharField(max_length=50)
 	type = models.ManyToManyField(DeviceType)
-	bus = models.ForeignKey(Bus, blank=True)
+	bus = models.ForeignKey(Bus, blank=True, null=True)
+
+	def __str__(self):
+		return self.name
 
 class Measure(models.Model):
 	type = models.ForeignKey(MeasureType)
@@ -29,8 +44,14 @@ class Measure(models.Model):
 	value = models.FloatField()
 	time = models.DateTimeField(default=now)
 
+	def __str__(self):
+		return str(self.value)
+
 class Sequence(models.Model):
 	name = models.CharField(max_length=20)
 	description = models.CharField(max_length=200, blank=True)
 	payload = models.CharField(max_length=100)
 	device = models.ManyToManyField(Device, blank=True)
+
+	def __str__(self):
+		return self.name
