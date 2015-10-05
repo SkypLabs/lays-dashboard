5076149603b7f1c96c7cd2e68580682811f009ca
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2015-10-05 23:53:48 +0200

    change admin site title

--- lays/settings.py
+++ lays/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'lays.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'dashboard/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
