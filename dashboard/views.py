19f15b6582dbe6e6defe85de6481e42cdd3984c0
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2015-10-15 20:17:51 +0200

    improve views processing

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,33 +3,46 @@ from django.conf import settings
 from .models import Measure, MeasureType, Device
 
 def index(request):
-	settings.USE_L10N = False
-
 	existing_device = Device.objects.exists()
-	types = MeasureType.objects.values_list('name', flat=True).distinct()
-	devices = Device.objects.values_list('name', flat=True).distinct()
-	data = dict()
 
-	for type in types:
-		data[type] = dict()
-		data[type]["total_count"] = Measure.objects.filter(type__name=type).count()
-		for device in devices:
-			data[type][device] = reversed(Measure.objects.filter(type__name=type).filter(device__name=device).order_by('-time')[:10])
+	if existing_device:
+		settings.USE_L10N = False
 
-	context = {
-		'existing_device' : existing_device,
-		'data' : data,
-	}
+		types = MeasureType.objects.values_list('name', flat=True).distinct()
+		devices = Device.objects.values_list('name', flat=True).distinct()
+		data = dict()
+
+		for type in types:
+			data[type] = dict()
+			data[type]["total_count"] = Measure.objects.filter(type__name=type).count()
+
+			for device in devices:
+				data[type][device] = reversed(Measure.objects.filter(type__name=type).filter(device__name=device).order_by('-time')[:10])
+
+		context = {
+			'existing_device' : existing_device,
+			'data' : data,
+		}
+	else:
+		context = {
+			'existing_device' : existing_device,
+		}
 
 	return render(request, 'dashboard/index.html', context)
 
 def rawdata(request):
 	existing_device = Device.objects.exists()
-	measure = Measure.objects.order_by('time').order_by('device').reverse()
 
-	context = {
-		'existing_device' : existing_device,
-		'measure' : measure,
-	}
+	if existing_device:
+		measure = Measure.objects.order_by('time').order_by('device').reverse()
+
+		context = {
+			'existing_device' : existing_device,
+			'measure' : measure,
+		}
+	else:
+		context = {
+			'existing_device' : existing_device,
+		}
 
 	return render(request, 'dashboard/rawdata.html', context)
