da3c4c70d297a123ce83156c3386b11d9d3bce06
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2015-11-01 22:23:56 +0100

    move 'AmqpClient' class to a separate file

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,28 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-import pika
 from .models import Measure, MeasureType, Device, Sequence
-
-class AmqpClient():
-	def __init__(self):
-		self.connection = pika.BlockingConnection(pika.ConnectionParameters(
-			'amqpserver'
-		))
-		self.channel = self.connection.channel()
-		self.channel.queue_declare(queue='task', durable=True)
-		self.channel.basic_qos(prefetch_count=1)
-
-	def send(self, body):
-		self.channel.basic_publish(
-			exchange='',
-			routing_key='task',
-			body=body,
-			properties=pika.BasicProperties(
-				delivery_mode=2
-			),
-		)
-
-		self.connection.close()
+from .amqp_client import AmqpClient
 
 @login_required
 def index(request):
